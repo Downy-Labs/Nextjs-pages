b50472de5aa31c9df0d5e0d08d73d25485fdfce1
laludonisetiawan <laludonisetiawan@gmail.com>
2023-10-14 14:31:03 +0800

    #1 page-router: membuat direktori product yang berisi index dan slug

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DetailProduct = () => {
+  return (
+    <div>
+      <h1>Detail Product</h1>
+      <p>Product: </p>
+    </div>
+  );
+};
+
+export default DetailProduct;
